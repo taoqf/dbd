ec128ccb57d7ab682c4a5e6cfdf37273e9579487
taoqf <tao_qiufeng@126.com>
2017-04-04 19:20:10 +0800

    remove typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="modules/express-serve-static-core/index.d.ts" />
-/// <reference path="modules/express/index.d.ts" />
-/// <reference path="modules/log4js/index.d.ts" />
-/// <reference path="modules/mime/index.d.ts" />
-/// <reference path="modules/serve-static/index.d.ts" />
